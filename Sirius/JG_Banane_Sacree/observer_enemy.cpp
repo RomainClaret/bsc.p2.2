5f05c6ad02b2cfd769eac45b108c04acd74e9cd1
Romain Claret <romain.claret@rocla.ch>
2015-09-03 16:39:41 +0200

    maps updates

--- Sirius/JG_Banane_Sacree/observer_enemy.cpp
+++ Sirius/JG_Banane_Sacree/observer_enemy.cpp
@@ -40,7 +40,7 @@ Observer_Enemy::~Observer_Enemy()
 void Observer_Enemy::addNPCObserver(C_Enemy* ennemi)
 {
     this->list_ennemisObserver.append(ennemi);
-    //hash_enemyPreviousState[ennemi] = ennemi->getEnemyState();
+    hash_enemyPreviousState[ennemi] = ennemi->getEnemyState();
 }
 
 void Observer_Enemy::removeNPCObserver(C_Enemy* ennemi)
@@ -175,7 +175,7 @@ void Observer_Enemy::switchAllToPause()
          hash_enemyPreviousState[enemy] = enemy->getEnemyState();
          enemy->changeState(new State_EnemySleep());
 
-         if(OldState != NULL){
+         if(OldState != NULL && hash_enemyPreviousState[enemy]  != OldState){
              delete OldState;
          }
     }
@@ -200,7 +200,11 @@ void Observer_Enemy::switchBackToState(QPoint phase)
             qDebug() << "---Switch back to a saved state " << phaseX << ", " << phaseY;
 
             State_Enemy* state = it.key()->changeState(it.value());
-            delete state;
+
+            if(state != it.value())
+            {
+                delete state;
+            }
         }
     }
 }
